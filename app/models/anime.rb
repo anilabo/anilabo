3cf64c93b6a8328d51779ec879c7bc450379e157
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-17 01:50:55 +0900

    feat: 性別とシーズンをenum化

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,3 +1,6 @@
 class Anime < ApplicationRecord
-  generate_public_uid
+  generate_public_uid generator: PublicUid::Generators::HexStringSecureRandom.new(20)
+
+  enum sex: { "men": 0, "women": 1, "custom": 2 }
+  enum season: { "winter": 1, "spring": 2, "summber": 3, "fall": 4 }
 end
