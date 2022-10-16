cf3b7aa8e7d783fb7904f06aef84fd233ccab8dd
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-16 22:58:48 +0900

    chore: devise, rails_admin, cancanの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   namespace :api do
     namespace :v1 do
       resources :animes, param: :public_uid
