d89cd84a6011ac772d39e19e95a98f81fa30183e
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-05 13:14:12 +0900

    feat: firebaseの情報をもとにユーザーを登録

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::UsersController < ApplicationController
+  include Firebase::Auth::Authenticable
+  skip_before_action :verify_authenticity_token
+
+  def create
+    raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+
+    user = User.create!(sign_up_params.merge(
+                          uid: payload['sub'],
+                          display_name: payload['name'],
+                          photo_url: payload['picture'],
+                          password: 'hogehoge',
+                          password_confirmation: 'hogehoge'
+                        ))
+    render json: user, status: :ok
+  end
+
+  private
+
+    def sign_up_params
+      params.require(:user).permit(:email)
+    end
+
+    def token_from_request_headers
+      request.headers['Authorization']&.split&.last
+    end
+
+    def token
+      params[:token] || token_from_request_headers
+    end
+
+    def payload
+      @payload ||= FirebaseIdToken::Signature.verify token
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Firebase::Auth::Authenticable
-
   # 400 Bad Request
   def response_bad_request
     render status: 400, json: { status: 400, message: 'Bad Request' }
