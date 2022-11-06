32d287dcd8d3a94140eac7ebbeddcfc73e76b405
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-07 01:53:43 +0900

    feat: watch_logsコントローラーのcreateアクションの作成

--- app/controllers/api/v1/watch_logs_controller.rb
+++ app/controllers/api/v1/watch_logs_controller.rb
@@ -1,5 +1,25 @@
 class Api::V1::WatchLogsController < ApplicationController
   before_action :logged_in_user!
+  before_action :set_anime
 
-  def create; end
+  def create
+    watch_log = logged_in_user.user_animes.new(watch_log_params)
+    watch_log.anime_id = @anime.id
+
+    if watch_log.save!
+      render json: logged_in_user, status: :ok
+    else
+      render json: watch_log.errors.full_messages # ここ動くか確認する。
+    end
+  end
+
+  private
+
+    def set_anime
+      @anime = Anime.find_by(public_uid: params[:anime_public_uid])
+    end
+
+    def watch_log_params
+      params.require(:watch_log).permit(:progress, :opinion, :finished_at)
+    end
 end
