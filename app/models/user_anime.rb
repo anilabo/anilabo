43ddd42e766f06cfae46bbfa5d46016d59b1ed1a
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-07 11:04:43 +0900

    feat: user_animeのprogressがwatched?のとき、opinionとfinished_atを必須にする

--- app/models/user_anime.rb
+++ app/models/user_anime.rb
@@ -2,4 +2,9 @@ class UserAnime < ApplicationRecord
   enum progress: { watched: 0, watching: 1, will_watch: 2 }
   belongs_to :user
   belongs_to :anime
+
+  with_options if: :watched? do
+    validates :opinion,        presence: true
+    validates :finished_at,    presence: true
+  end
 end
