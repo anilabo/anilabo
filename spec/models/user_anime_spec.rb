dd6a57f2aeb1479600dc7b0be38780531e2b22e7
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-14 14:48:25 +0900

    test: is_spoilerに関するテストをuser_animeのモデルspecに追加

--- spec/models/user_anime_spec.rb
+++ spec/models/user_anime_spec.rb
@@ -21,19 +21,22 @@ RSpec.describe UserAnime, type: :model do
 
     context 'progressがwatched以外のとき' do
       before do
-        @watch_log = user.user_animes.new(anime_id: anime.id, progress: "watching", opinion: "まだ面白くない", finished_at: "2022-11-13")
+        @watch_log = user.user_animes.new(anime_id: anime.id, progress: "watching", opinion: "まだ面白くない", finished_at: "2022-11-13", is_spoiler: true)
       end
 
-      it 'opinion, finished_atがnilで保存されること' do
+      it 'opinion, finished_at, is_spoilerがnilで保存されること' do
         @watch_log.save!
         expect(@watch_log.opinion).to eq(nil)
         expect(@watch_log.finished_at).to eq(nil)
-        @watch_log.update!(progress: "watched", opinion: '面白かった！', finished_at: '2022-11-14')
+        expect(@watch_log.is_spoiler).to eq(false)
+        @watch_log.update!(progress: "watched", opinion: '面白かった！', finished_at: '2022-11-14', is_spoiler: true)
         expect(@watch_log.opinion).to eq("面白かった！")
         expect(@watch_log.finished_at).to eq('2022-11-14 00:00:00.000000000 +0000')
+        expect(@watch_log.is_spoiler).to eq(true)
         @watch_log.update!(progress: "will_watch", opinion: '面白そう', finished_at: '2022-11-15')
         expect(@watch_log.opinion).to eq(nil)
         expect(@watch_log.finished_at).to eq(nil)
+        expect(@watch_log.is_spoiler).to eq(false)
       end
     end
   end
