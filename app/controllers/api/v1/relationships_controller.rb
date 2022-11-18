38ee21ece19805fd4276a4fca76f812b19e3678f
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-18 22:48:21 +0900

    fix: relationコントローラーのディレクトリ修正

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -1,9 +1,10 @@
-class RelationshipsController < ApplicationController
+class Api::V1::RelationshipsController < ApplicationController
   before_action :logged_in_user!
   before_action :set_target_user
 
   def create
     logged_in_user.follow(@target_user.id)
+
     render json: logged_in_user, status: :ok
   end
 
