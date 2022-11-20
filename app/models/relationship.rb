b6e40c5f2a77a96fa4fa95798063f14b81b11c9d
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-21 00:29:50 +0900

    feat: Relationshipに関するNotificationのコールバックを設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,9 +5,22 @@ class Relationship < ApplicationRecord
   validate :different_by_target
   validates :followed_id, uniqueness: { scope: :follower_id }
 
+  after_save :create_notification
+  after_destroy :delete_notification
+
   private
 
     def different_by_target
       errors.add(:base, 'followed_id and follower_id must be different.') if followed_id == follower_id
+    end
+
+    def create_notification
+      Notification.create!(operative_user_id: follower_id, passive_user_id: followed_id, action: 'follow')
+      Notification.create!(operative_user_id: followed_id, passive_user_id: follower_id, action: 'followed')
+    end
+
+    def delete_notification
+      Notification.find_by!(operative_user_id: follower_id, passive_user_id: followed_id, action: 'follow').destroy!
+      Notification.find_by!(operative_user_id: followed_id, passive_user_id: follower_id, action: 'followed').destroy!
     end
 end
