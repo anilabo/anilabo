50621086f164cfedd2b115b389de32d76390d5ca
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-20 16:23:00 +0900

    feat: userとnotificationの繋ぎ込み

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,6 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
+  belongs_to :operative_user, class_name: 'User', foreign_key: 'operative_user_id', optional: true
+  belongs_to :passive_user, class_name: 'User', foreign_key: 'passive_user_id', optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'operative_user_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'passive_user_id', dependent: :destroy
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
