d942aba59c43e100d414a9e4bc0621cec4aa012c
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-22 22:47:19 +0900

    chore: kaminari導入

--- app/controllers/api/v1/timelines_controller.rb
+++ app/controllers/api/v1/timelines_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TimelinesController < ApplicationController
+  include Pagenation
+
   def show
     user = User.find_by(email: 'hm385.chejptks@gmail.com')
 
@@ -7,7 +9,11 @@ class Api::V1::TimelinesController < ApplicationController
                     .includes(%i[watch_log anime operative_user passive_user])
                     .where(operative_user_id: array)
                     .order(created_at: :desc)
+                    .page(params[:page])
+                    .per(10)
 
-    render json: notifications, status: :ok
+    pagenation = pagenation(notifications)
+
+    render json: notifications, meta: pagenation, status: :ok
   end
 end
