ab3454d2106de39ecf11e16dc891d492e7d133a8
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-23 10:56:01 +0900

    test: userのフォロー機能に関するアソシエーションのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,53 +5,79 @@ RSpec.describe User, type: :model do
   let(:users)   { create_list(:provisional_user, 10) }
   let(:animes)  { create_list(:provisional_anime, 10)}
 
-  before do
-    20.times.each do |n|
-      progress = ["watched", "will_watch", "watching"].sample
-      if progress == "watched"
-        user.user_animes.create!(anime_id: animes.sample.id, progress: progress, opinion: "hogehoge", finished_at: "2022-11-23")
-      else
-        user.user_animes.create!(anime_id: animes.sample.id, progress: progress)
-      end
-    end
-  end
-
+  
   describe "アソシエーションに関するテスト" do
-    context "watched_animesは" do
-      it "中間テーブルのprogressがwatchedであること" do
-        expect(user.watched_logs.first.progress).to eq("watched")
+    describe "サブスクライブ機能" do
+      before do
+        20.times.each do |n|
+          progress = ["watched", "will_watch", "watching"].sample
+          if progress == "watched"
+            user.user_animes.create!(anime_id: animes.sample.id, progress: progress, opinion: "hogehoge", finished_at: "2022-11-23")
+          else
+            user.user_animes.create!(anime_id: animes.sample.id, progress: progress)
+          end
+        end
       end
 
-      it "アニメの配列であること" do
-        expect(user.watched_animes.first.title).to include("anime")
+      context "watched_animesは" do
+        it "中間テーブルのprogressがwatchedであること" do
+          expect(user.watched_logs.first.progress).to eq("watched")
+        end
+
+        it "アニメの配列であること" do
+          expect(user.watched_animes.first.title).to include("anime")
+        end
+      end
+
+      context "watching_animesは" do
+        it "中間テーブルのprogressがwatchingであること" do
+          expect(user.watching_logs.first.progress).to eq("watching")
+        end
+
+        it "アニメの配列であること" do
+          expect(user.watched_animes.first.title).to include("anime")
+        end
+      end
+
+      context "will_watch_animesは" do
+        it "中間テーブルのprogressがwill_watchであること" do
+          expect(user.will_watch_logs.first.progress).to eq("will_watch")
+        end
+
+        it "アニメの配列であること" do
+          expect(user.watched_animes.first.title).to include("anime")
+        end
       end
     end
 
-    context "watching_animesは" do
-      it "中間テーブルのprogressがwatchingであること" do
-        expect(user.watching_logs.first.progress).to eq("watching")
+    describe "フォロー機能" do
+      before do
+        Relationship.create(follower_id: user.id, followed_id: users.sample.id)
+        Relationship.create(follower_id: users.sample.id, followed_id: user.id)
       end
 
-      it "アニメの配列であること" do
-        expect(user.watched_animes.first.title).to include("anime")
-      end
-    end
-
-    context "will_watch_animesは" do
-      it "中間テーブルのprogressがwill_watchであること" do
-        expect(user.will_watch_logs.first.progress).to eq("will_watch")
+      context "followingは" do
+        it "ユーザーの配列であること" do
+          expect(user.followings.first.email).to include('@example.com')
+        end
       end
 
-      it "アニメの配列であること" do
-        expect(user.watched_animes.first.title).to include("anime")
+      context "followersは" do
+        it "ユーザーの配列であること" do
+          expect(user.followers.first.email).to include('@example.com')
+        end
       end
     end
   end
+
+  # describe "メソッドに関するテスト" do
+  # end
 
   # describe "バリデーションに関するテスト" do
   # end
 
   # describe "コールバックに関するテスト" do
-
   # end
+
+
 end
