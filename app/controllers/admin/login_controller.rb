d595ffe9d469c506d773f9759f01c838438659c0
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-25 11:38:45 +0900

    refactor: bcryptのバリデーションをオフにし、ログイン時のリダイレクト先を修正

--- app/controllers/admin/login_controller.rb
+++ app/controllers/admin/login_controller.rb
@@ -3,12 +3,8 @@ class Admin::LoginController < Admin::ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to '/admin'
-    else
-      redirect_to '/'
-    end
+    session[:user_id] = user.id if user&.authenticate(params[:password])
+    redirect_to '/'
   end
 
   def destroy
