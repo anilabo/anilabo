09ec01e67b84678f3b348ec6a8c095371702b053
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-27 11:19:38 +0900

    test: Notificationのバリデーションを追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -12,4 +12,16 @@ class Notification < ApplicationRecord
   belongs_to :passive_user, class_name: 'User', foreign_key: 'passive_user_id', optional: true
   belongs_to :anime, optional: true
   belongs_to :watch_log, class_name: 'UserAnime', foreign_key: 'watch_log_id', optional: true
+
+  validates :action, presence: true
+  validates :operative_user_id, presence: true
+
+  with_options if: -> { follow? } do
+    validates :passive_user_id, presence: true
+  end
+
+  with_options if: -> { watching? || will_watch? || opinion? } do
+    validates :anime_id,        presence: true
+    validates :watch_log_id,    presence: true
+  end
 end
