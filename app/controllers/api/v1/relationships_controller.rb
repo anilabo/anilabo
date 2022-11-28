1a8557799896e988d50f19dc84367544decfb70a
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-28 11:19:11 +0900

    test: relationshipコントローラーのテストを追加

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::RelationshipsController < Api::ApplicationController
 
   def create
     current_user.follow(@target_user)
-
     render json: current_user, status: :ok
   end
 
@@ -16,6 +15,7 @@ class Api::V1::RelationshipsController < Api::ApplicationController
   private
 
     def set_target_user
-      @target_user = User.find_by!(uid: params[:user_uid])
+      @target_user = User.find_by(uid: params[:user_uid])
+      response_bad_request if @target_user.nil?
     end
 end
