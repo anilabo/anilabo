b15f77f0dce513af67f1ea9dbfcf5333db97ef62
hulk-higakijin <hm385.chejptks@gmail.com>
2022-11-29 23:19:01 +0900

    test: users#updateのrspecを追加

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "Api::V1::Users", type: :request do
 
   describe "GET /api/v1/users/:uid" do
     context "該当するユーザーが存在するとき" do
-
       it "ステータス200を返すこと" do
         get api_v1_user_path(valid_params)
         expect(response).to have_http_status(200)
@@ -18,6 +17,36 @@ RSpec.describe "Api::V1::Users", type: :request do
       it "ステータス404を返すこと" do
         get api_v1_user_path(invalid_params)
         expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe "PATCH /api/v1/users/:uid" do
+    context "未ログイン時" do
+      it "ステータス401を返すこと" do
+        patch api_v1_user_path({uid: user.uid})
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "ログイン時" do
+      before { stub_firebase(user) }
+
+      it "ステータス200を返すこと" do
+        patch api_v1_user_path({uid: user.uid, user: { display_name: "newName!" }})
+        expect(response).to have_http_status(200)
+      end
+
+      it "Userモデルのレコード数が変化しないこと" do
+        expect do
+          patch api_v1_user_path({uid: user.uid, user: { display_name: "newName!" }})
+        end.to change { User.count}.by(0)
+      end
+
+      it "current_userのdisplay_nameが更新されること" do
+        expect do
+          patch api_v1_user_path({uid: user.uid, user: { display_name: "newName!" }})
+        end.to change { user.display_name }.from('野生のミシシッピズワイガニ').to('newName!')
       end
     end
   end
