f1f6a354345790b957531cf5c91222f7117ac736
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-05 02:17:07 +0900

    test: users#destroyのテストを追加

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -50,4 +50,34 @@ RSpec.describe "Api::V1::Users", type: :request do
       end
     end
   end
+
+  describe "DELETE /api/v1/users/:uid" do
+    context "未ログイン時" do
+      it "ステータスコード401を返すこと" do
+        delete api_v1_user_path(user)
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "ログイン時" do
+      before { stub_firebase(user) }
+
+      it "ステータスコード200を返すこと" do
+        delete api_v1_user_path(user)
+        expect(response).to have_http_status(200)
+      end
+
+      it "ユーザーのレコード数が1つ減ること" do
+        expect do
+          delete api_v1_user_path(user)
+        end.to change { User.count }.by(-1)
+      end
+
+      it "該当ユーザーが見つからないこと" do
+        expect(user.display_name).to eq('野生のミシシッピズワイガニ')
+        delete api_v1_user_path(user)
+        expect(User.find_by(display_name: '野生のミシシッピズワイガニ')).to eq(nil) 
+      end
+    end
+  end
 end
